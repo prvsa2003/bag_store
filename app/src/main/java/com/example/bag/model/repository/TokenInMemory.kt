7f916ca7353aef940176bfbf04dca51b2a76105f
ali <parviziali81@yahoo.com>
2023-10-28 22:08:50 +0330

    set refreshtoken function

--- app/src/main/java/com/example/bag/model/repository/TokenInMemory.kt
+++ app/src/main/java/com/example/bag/model/repository/TokenInMemory.kt
@@ -2,6 +2,16 @@ package com.example.bag.model.repository
 
 object TokenInMemory {
     var username : String?=null
+        private set
+
+    var token : String ? = null
+        private set
+
+
+    fun refreshtoken(username:String , newtoken : String){
+        this.username = username
+        this.token = newtoken
+    }
     
 
 }
